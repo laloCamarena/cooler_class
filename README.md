3e3c024a4d5cbc669128788650705017e67c953b
Eduardo Camarena <lalo.a.camarena@gmail.com>
2020-07-20 20:44:19 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+A package to make your uni clases better
+
+Created by Eduardo Atonatiuh Camarena Santamaría and Diego Salvador Gonzales Camacho
+
+To install project you need to install cuda with the following command: cuda install cudatoolkit
+next you need to install the requirements.txt file with the command: pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='cooler-class',
+    version='0.0.1',
+    description='Live online classes',
+    author='Eduardo Camarena Santamaria',
+    author_email='lalo.a.camarena@gmail.com',
+    package_dir={'': 'src'},
+    packages=['cooler_class'],
+    install_requires=[
+        'numpy', 'sqlalchemy', 'pandas', 'seaborn',
+        'mysqlclient', 'opencv-python', 'flask',
+        'flask-sqlalchemy', 'flask-restful',
+        'PyQt5==5.13.0', 'pyqt5-tools', 'pytest'
+    ]
+)
